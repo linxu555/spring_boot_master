371a39b524c92d1224e3d6867c56c92a8ce78401
linxueqin <275468520@qq.com>
2018-11-23 18:56:05 +0800

    1.Aop切面实现记录日志； 2.log4j日志配置；

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,9 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.util.MyException;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: linxueqin
@@ -19,5 +18,12 @@ public class HelloController {
     @RequestMapping(value = "/json", method = RequestMethod.POST)
     public String hello() throws MyException{
         throw new MyException("发生错误");
+    }
+
+    @ApiOperation(value="aop切面测试", notes="web请求做切面来记录日志")
+    @RequestMapping(value = "/aopTest",method = RequestMethod.GET)
+    @ResponseBody
+    public String aopTest(@RequestParam @ApiParam(name = "name", value = "名称", required = true)String name){
+        return "hello aop:"+name;
     }
 }
